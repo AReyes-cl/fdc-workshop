0d88469aa02308d769d9d5bcc56f7e2495ef2847
The Cowboy Coder <sergiob@lagash.com>
2017-08-05 09:53:14 -0400

    hola mundo

---
+++ app.js
@@ -0,0 +1,35 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+server.get('/api/test', (req, res, next) => {
+  console.log("We are ok!"); 
+  res.send('We are ok!');
+  next();
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector);
+
+bot.dialog("/", (session) => {
+        session.sendTyping();
+        session.send("Hola mundo!");
+    });
+
+
+
+
